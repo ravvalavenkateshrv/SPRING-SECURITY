7cc5ad26f3027082cb0b8c5ef6b8311e087b227f
ravvalavenkateshrv <ravvalavenkateshrv@gmail.com>
2025-08-21 18:59:40 +0530

    Add files via upload

---
+++ spring_sign_up_project/src/main/java/com/net/SpringSignUpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSignUpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSignUpProjectApplication.class, args);
+	}
+
+}
