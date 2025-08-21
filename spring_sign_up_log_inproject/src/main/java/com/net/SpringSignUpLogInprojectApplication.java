49ff66485020b68cd42ce116c2d67080b3986d99
ravvalavenkateshrv <ravvalavenkateshrv@gmail.com>
2025-08-21 19:02:14 +0530

    Add files via upload

---
+++ spring_sign_up_log_inproject/src/main/java/com/net/SpringSignUpLogInprojectApplication.java
@@ -0,0 +1,13 @@
+package com.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSignUpLogInprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSignUpLogInprojectApplication.class, args);
+	}
+
+}
---
+++ spring_sign_up_log_inproject/src/test/java/com/net/SpringSignUpLogInprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSignUpLogInprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
